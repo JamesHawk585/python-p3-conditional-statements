8458c6a23935c2c67e218ac4d65ec6d20691f41d
James Hawk <jph94880@gmail.com>
2023-07-12 21:48:28 -0500

    Passes two tests

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    try:
+        if username == 'admin' or username == 'ADMIN' and password == '12345':
+            return 'Access granted'
+        else:
+             return 'Access denied'
+    except: 
+        return 'An Error Occured'
+# Write a function admin_login() that takes two arguments, a username and a password. If the username is "admin" or "ADMIN" and the password is "12345", return "Access granted". Otherwise, return "Access denied".
+
 
 def hows_the_weather(temperature):
     # your code here
